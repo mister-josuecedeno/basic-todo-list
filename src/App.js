ad128e0225fba6fb433349240b45d36640f8cda4
josuecedeno <josuecedeno@yahoo.com>
2021-01-20 16:39:34 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
+  // example object { id: 1, name: 'Todo 1', complete: true },
+  const [todos, setTodos] = useState([]);
+
+  // ref provides the value of the input
+  const todoNameRef = useRef(null);
+
+  // Retrieve existing todos from local storage
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  // Add todos to local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  // Toggle current state of the todo
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]; // use a copy of the todos
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  };
+
+  // Add a new todo
+  const handleAddTodo = (e) => {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  // Filter for incomplete todos
+  const handleClearTodos = () => {
+    const newTodos = [...todos].filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type='text' />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed Todos</button>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
